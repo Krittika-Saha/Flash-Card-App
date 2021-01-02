52aa6308f6b6d8ad1cbf13dbac9bc0fff6fd5030
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-01-02 19:05:23 +0530

    Completed Flashy

--- main.py
+++ main.py
@@ -1,51 +1,71 @@
 from tkinter import *
-import pandas as pds
-from random import randint
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
-chosen_word = ''
+current_card = {}
+to_learn = {}
 
-#----------------------------RIGHT AND WRONG--------------
-
-
-def change_french():
-    global chosen_word
-    chosen_word = randint(0, 100)
-    data = pds.read_csv("data/french_words.csv")
-    canvas.itemconfig(front_img, image=image)
-    canvas.itemconfig(title, text='French', fill='black')
-    canvas.itemconfig(content, text=data['French'][chosen_word], fill='black')
-    window.after(3000, change_english)
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
-def change_english():
-    data = pds.read_csv("data/french_words.csv")
-    canvas.itemconfig(front_img, image=back_img)
-    canvas.itemconfig(title, text='English', fill='white')
-    canvas.itemconfig(content, text=data['English'][chosen_word], fill='white')
-#----------------------------UI SETUP--------------------------
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
 window.title("Flashy")
-window.minsize(width=900, height=550)
-window.configure(bg=BACKGROUND_COLOR, padx=50, pady=50)
-#Canvas
-canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-image = PhotoImage(file='C:/Flash-Card-App/images/card_front.png')
-back_img = PhotoImage(file='C:/Flash-Card-App/images/card_back.png')
-front_img = canvas.create_image(400, 263, image=image)
-#Text
-title = canvas.create_text(400, 150, text='Are you ready?', font=('Ariel', 40, 'italic'))
-content = canvas.create_text(400, 263, text='Click ✔ to start', font=("Ariel", 60, 'bold'))
-#Buttons
-unknown_button_img = PhotoImage(file='C:/Flash-Card-App/images/wrong.png')
-unknown_button = Button(image=unknown_button_img, highlightthickness=0)
-unknown_button.grid(row=1, column=0)
-known_button_img = PhotoImage(file='images/right.png')
-known_button = Button(image=known_button_img, highlightthickness=0, command=change_french)
-known_button.grid(row=1, column=1)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
+
 window.mainloop()
+
+
 
